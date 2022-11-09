bfb6b6442cb3de4cf126359b53a3ad51bc1be212
Meryem Özbalık <88854043+meyrosd@users.noreply.github.com>
2022-11-09 10:31:39 +0300

    animals and food game
    beginner level game made with unity.

---
+++ animal and food/Assets/Script/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange=10;
+
+    public GameObject ProjectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if(Input.GetKeyDown(KeyCode.Mouse0))  //Launch a projectile from the player
+        {
+            Instantiate(ProjectilePrefab, transform.position, ProjectilePrefab.transform.rotation); //tests if the player presses spacebar
+        }
+    }
+}
